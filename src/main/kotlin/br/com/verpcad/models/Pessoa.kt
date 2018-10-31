8b9ab7b6eb6d1f5b233fab5ec3a7535596eb6dc7
Victor Emidio Rosado De Paula <vepaula@latam.stefanini.com>
2018-10-31 13:08:58 -0300

    removing nullable from dataNascimento

--- src/main/kotlin/br/com/verpcad/models/Pessoa.kt
+++ src/main/kotlin/br/com/verpcad/models/Pessoa.kt
@@ -13,7 +13,7 @@ class Pessoa {
     var id: Long? = null
     @Column(name = "nome", nullable = false)
     var nome: String? = null
-    @Column(name = "data_nascimento", nullable = false)
+    @Column(name = "data_nascimento")
     var dataNasciment: Date? = null
     @Column(name = "cpf", length = 14, nullable = false)
     var cpf: String? = null
--- src/main/kotlin/br/com/verpcad/resources/PessoaController.kt
+++ src/main/kotlin/br/com/verpcad/resources/PessoaController.kt
@@ -1,29 +1,23 @@
-package br.com.verpcad.controller
+package br.com.verpcad.resources
 
 import br.com.verpcad.models.Pessoa
 import br.com.verpcad.repositories.PessoaRepository
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import java.util.*
 
 
 @RestController
 @RequestMapping("pessoa")
-class PessoController {
+class PessoaController {
 
     @Autowired
     lateinit var repository: PessoaRepository
 
-    @GetMapping
-    fun list(): Boolean {
-        var pessoa = Pessoa()
-        pessoa.id = 1
-        pessoa.cpf = "123123"
-        pessoa.dataNasciment = Date()
-        pessoa.nome = "victor"
-        repository.save(pessoa)
-        return true
+    @PostMapping
+    fun salvarPessoa(@RequestBody pessoa: Pessoa): Pessoa {
+        return repository.save(pessoa)
     }
 }
